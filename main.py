21e20a3732a3274238ea0c7e522c0e0c2a9f705b
zulinsyhk <77933144+zulinsyhk@users.noreply.github.com>
2023-11-06 09:19:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 # Dummy data, gantilah dengan data sesuai kebutuhan
 all_df = pd.read_csv("all_dataset.csv")
 
